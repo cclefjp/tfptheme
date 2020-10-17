7ec85905bc3924f5ba332588dc67ae60ae3abb70
cclef <watadani@tsukubane.local>
2020-10-17 14:36:09 +0900

    copyright_statement項目を追加

--- tfp/tfp-settings.php
+++ tfp/tfp-settings.php
@@ -20,7 +20,7 @@
  /* 設定ページ内のフィールドの作成 */
 add_action('admin_init', 'register_tfp_settings');
 function register_tfp_settings() {
-    $tfp_options = ['logo_img'];
+    $tfp_options = ['logo_img', 'copyright_statement'];
     foreach($tfp_options as $tfp_option) {
         register_setting('tfp_settings', 'tfp_' . $tfp_option);
     }
