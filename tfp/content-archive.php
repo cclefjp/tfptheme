9dc874c111b4e9278faf1a56d3298afe11d8a28f
cclef <watadani@tsukubane.local>
2020-10-21 11:23:12 +0900

    アイキャッチ画像を貼れるように修正

--- tfp/content-archive.php
+++ tfp/content-archive.php
@@ -1,10 +1,16 @@
 <!-- 投稿一覧ページ内での各投稿の表示を記載 -->
 
 <a class="post-link" href="<?php the_permalink(); ?>">
-  <div class="post-body">
+  <div class="post-body" style="
+    background: url(<?php echo tfp_get_post_background(); ?>) no-repeat;
+    background-size: cover;
+
+    ">
     <span class="post-line">
         <span  class="title"><?php the_title(); ?></span>
         <time class="release">(<?php the_time('Y.m.d'); ?>)</time>
     </span>
+
   </div>
 </a>
+
