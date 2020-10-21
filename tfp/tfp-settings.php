9aefb3898a46e1cff992b10f1a5826383ce54f26
cclef <watadani@tsukubane.local>
2020-10-21 18:26:04 +0900

    記事一覧背景の設定を追加

--- tfp/tfp-settings.php
+++ tfp/tfp-settings.php
@@ -22,7 +22,7 @@ add_action('admin_init', 'register_tfp_settings');
 function register_tfp_settings() {
     $tfp_options = ['logo_img',
                     'default_header_img',
-                    'default_post_img',
+                    'default_post_background',
                     'header_font_color',
                     'webfonts',
                     'title_fontfamily',
